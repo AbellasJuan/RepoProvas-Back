014c6fcc02eb0e25955e4116238fa78d9d6bd160
Juan Abellás <82621417+AbellasJuan@users.noreply.github.com>
2022-04-29 14:08:52 -0300

    Update teacherController.ts

--- src/controllers/teacherController.ts
+++ src/controllers/teacherController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { Teacher } from "@prisma/client";
 import teacherService from "../services/teacherService.js";
-// import { Category, Test, Teacher, Term, Discipline, TeacherDiscipline } from "@prisma/client";
 
 export async function findAll(req: Request, res: Response){
   const teachers: Teacher[] = await teacherService.findAll();
